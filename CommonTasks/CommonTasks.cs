83a2bbddb818225fec1fa15d0db2a1e5fab6a129
Yurii Matora <yura.matora11@gmail.com>
2021-06-12 22:55:36 +0300

    Initial commit

---
+++ CommonTasks/CommonTasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Common.Tasks
+{
+    public class CommonTasks
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Pow(int a, int b)
+        {
+            return (int) Math.Pow(a, b);
+        }
+    }
+}
